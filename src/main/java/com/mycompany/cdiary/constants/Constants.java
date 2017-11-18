7b67e89831e1625281f74aac3cabb17b4a4f871e
Kimiko Ishii <pom96egran46ate@gmail.com>
2017-11-18 21:53:12 +0900

    [change]gitignore

--- src/main/java/com/mycompany/cdiary/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.cdiary.constants;
-
-public class Constants {
-    public static final String USER_KEY = "user";
-    public static final String ErrMsg_Key = "errorMsg";
-    public static final String ErrTitle_Key = "errorTitle";
-}
